d51153ec36d02fcfa6c00f1458c0a2ecca749168
Dannyjpeg <nyeldf2007@gmail.com>
2024-12-03 08:57:27 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const userController = require('./Controllers/userController'); 
+const app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.get('/', userController.getAllUsers);
+app.get('/edit/:id', userController.getUserById);
+app.post('/edit/:id', userController.updateUser); 
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+});
